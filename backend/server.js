52d2eddc2318a8dca433a3e137f14524bd94f8cb
sawnjordan <sawnjordan@gmail.com>
2023-07-05 17:56:02 +0545

    Created server listening to PORT

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
 const http = require("http");
+
+const server = http.createServer();
+
+const PORT = 5000;
+
+server.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`Server listening to PORT:${PORT}`);
+    console.log(`Browser: http://localhost:${PORT} `);
+    console.log(`Press CTRL + C to STOP the server`);
+  } else {
+    console.log(err);
+  }
+});
