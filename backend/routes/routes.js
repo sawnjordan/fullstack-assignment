d9503c8c1811b15450aad489f4f7c1c75df377df
sawnjordan <sawnjordan@gmail.com>
2023-07-07 11:41:45 +0545

    redoing auth module

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 const bookRoutes = require("../modules/book/book.routes");
-const authRoutes = require("../modules/auth/auth.routes");
+// const authRoutes = require("../modules/auth/auth.routes");
 
-router.use("/auth", authRoutes);
+// router.use("/auth", authRoutes);
 router.use("/admin/books", bookRoutes);
 
 module.exports = router;
