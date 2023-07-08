41803ac80209c8f042195b6bcf7b7a0b55820946
sawnjordan <sawnjordan@gmail.com>
2023-07-08 08:44:47 +0545

    added order model

---
+++ backend/modules/order/order.model.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    books: [
+      {
+        book: {
+          type: mongoose.Types.ObjectId,
+          ref: "Book",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    paymentInfo: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+    },
+    paidAt: {
+      type: Date,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+    shippingAddress: {
+      address: {
+        type: String,
+        required: true,
+      },
+      phoneNumber: {
+        type: String,
+        required: true,
+      },
+    },
+    orderStatus: {
+      type: Number,
+      required: true,
+      default: "Proecessing",
+    },
+  },
+  {
+    timestamps: true,
+    autoCreate: true,
+    autoIndex: true,
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
