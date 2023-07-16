f8748745f1ca7e195289d2c749244cf78e184bf0
sawnjordan <sawnjordan@gmail.com>
2023-07-16 18:57:25 +0545

    created react app and added header and footer component

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Header } from "./components/layout/header";
+import { Footer } from "./components/layout/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
